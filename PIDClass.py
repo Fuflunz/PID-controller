22cd1e1333c356a273e5b2b9faf8cdfad08b1c4b
Fuflunz <Amadeus_Dionysos_Sagner@outlook.de>
2025-08-11 11:32:58 +0200

    Update PIDClass.py

--- PIDClass.py
+++ PIDClass.py
@@ -7,43 +7,49 @@ class PID:
     dT = 0.
     count = 0.
     Int = 0.
-    range = 0.
-
-    def __init__(self, dt, kp, kd, ki, dT, range):
-        self.dt = dt  # dt = Zeit in s
-        self.kp = kp  # kp = Proportionalkonstante
-        self.kf = kd  # kd = Differentialkonstante
-        self.ki = ki  # ki = Integralkonstante
-        self.dT = dT  # dT = Integrationszeit in s
-        self.range = range  # range, ab wann Differentialteil eingreifen soll
-        # range ist in der Unit des zu korrigierenden Wertes
-
-    def correct(self, set, act):
+    
+    def __init__(self, dt, kp, kd, ki, dT, Offset):   
+        self.dt = dt                                 # dt = Zeit in s
+        self.kp = kp                                 # kp = Proportionalkonstante
+        self.kf = kd                                 # kd = Differentialkonstante
+        self.ki = ki                                 # ki = Integralkonstante
+        self.dT = dT                                 # dT = Integrationszeit in s
+        self.Offset = Offset
+        self.err = 0
+        
+    def correct(self, set, act, range1, range2, range3, range4, Out_max, Out_min):
         e = set - act
-
+        
         P = self.kp * e
-
-        if (self.err < self.range):
-            if (self.count * self.dt == self.dT):
-                I = self.Int
-                self.count = 0
-                self.Int = 0.
-                self.Int += self.ki * e * self.dt
-                self.count += 1
-                # print("Integrate")
-                # print(self.count)
-            else:
-                I = 0.
-                self.Int += self.ki * e * self.dt
-                self.count += 1
-                # print(self.count)
-
-        if (self.err < self.range):
-            D = -self.kd * ((e - self.err) / self.dt)
+        
+        if(range2 < act) and (act < range1):
+            print("in Range (Int)")
+            self.Int += self.err
+            I = self.ki * self.dt * self.Int
+        else:
+            self.Int = 0
+            I = 0.
+            
+        if(range4 < act < range3):
+            print("in Range (Diff)")
+            D = -self.kd * ((e-self.err)/(self.dt))
         else:
             D = 0.
-
-        out = P + I + D
-
+            
+        out = P+I+D+self.Offset
+        if(out > Out_max):
+            o = Out_max
+        elif(out<Out_min):
+            o = Out_min
+        else:
+            o = out
+        
+        print("P")
+        print(P)
+        print("I")
+        print(I)
+        print("D")
+        print(D)
+        
         self.err = e
-        return out 
+        return o
